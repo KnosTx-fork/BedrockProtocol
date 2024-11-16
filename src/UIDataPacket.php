5b2fa44119390e1804a38c676f386b075881f748
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-16 18:58:27 +0700

    Update UIDataPacket.php

--- src/UIDataPacket.php
+++ src/UIDataPacket.php
@@ -19,15 +19,13 @@ use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 class UIDataPacket extends Packet {
 	public const NETWORK_ID = ProtocolInfo::UI_DATA_PACKET;
 
-	/** @var string */
 	public string $formId;
 
-	/** @var string */
 	public string $formData;
 
 	/** 
 	 * Decode payload from client packet
-	 */
+ 	 */
 	protected function decodePayload(PacketSerializer $in) : void {
 		$this->formId = $in->getString();
 		$this->formData = $in->getString();
@@ -43,7 +41,7 @@ class UIDataPacket extends Packet {
 
 	/**
 	 * Get the network ID of the packet
-	 */	
+ 	 */	
 	public function getNetworkId() : int{
 		return self::NETWORK_ID;
 	}
